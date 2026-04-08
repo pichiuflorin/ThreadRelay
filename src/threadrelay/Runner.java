cfb4e529ec747bd7fbe65009f84b00c2ca0d11ae
pichiuflorin <pichiu.florin@franchettisalviani.net>
2026-04-08 11:29:24 +0200

    Creazione della classe Runner

---
+++ src/threadrelay/Runner.java
@@ -0,0 +1,16 @@
+package threadrelay;
+
+public class Runner implements Runnable{
+    public Runner() {
+        //risorsa condivisa
+        boolean hasTestimone = false;
+        //quota che deve raggiungere 90
+        int quota = 0;
+    }
+
+    public void run(){
+        while (quota < 90) {
+
+        }
+    }
+}
